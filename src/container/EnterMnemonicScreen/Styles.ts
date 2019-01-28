1df6d042c823fd51461e3e3d0c53e8f124965009
DianaLeee <yeonylee94@gmail.com>
2019-01-28 15:03:41 +0900

    ENH: 니모닉 입력 UI 추가

--- src/container/EnterMnemonicScreen/Styles.ts
+++ src/container/EnterMnemonicScreen/Styles.ts
@@ -6,5 +6,25 @@ export default StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+  button: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  createButton: {
+    width: 250,
+    height: 40,
+    backgroundColor: "#030066",
+    margin: 15
+  },
+  mnemonicContainer: {
+    flex: 0.3,
+    flexDirection: "column",
+    width: "90%",
+    height: "30%",
+    margin: 10,
+    backgroundColor: "#fff"
   }
 });
--- src/container/EnterMnemonicScreen/index.tsx
+++ src/container/EnterMnemonicScreen/index.tsx
@@ -1,27 +1,49 @@
 import React from "react";
-import { View } from "react-native";
+import { View, AsyncStorage } from "react-native";
 import { route } from "../../constants/route";
-import { Button, Text } from "react-native-paper";
+import { Button, Text, Title, TextInput } from "react-native-paper";
 import { NavigationScreenProps } from "react-navigation";
 import styles from "./Styles";
 
-export default class EnterMnemonicScreen extends React.Component<NavigationScreenProps> {
+export default class EnterMnemonicScreen extends React.Component<
+  NavigationScreenProps
+> {
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is Mnemonic Backup Page</Text>
+        <Title>Mnemonic Recovery</Title>
+        <Text>Please enter ther correct Mnemonic of your wallet</Text>
+        <TextInput
+          style={styles.mnemonicContainer}
+          mode={"outlined"}
+          multiline={true}
+          onChangeText={value => this.saveMnemonic(value)}
+        />
         <Button
-          icon="add-a-photo"
+          style={styles.createButton}
           mode="contained"
-          onPress={this.navigateToBackUpMnem}
+          onPress={this.navigateToNextPage}
         >
-          니모닉 입력 완료
+          Recover Wallet
         </Button>
       </View>
     );
   }
 
-  navigateToBackUpMnem = () => {
-    this.props.navigation.navigate(route.BACKUP_MNEMONIC_SCREEN);
+  state = {
+    visible: true,
+    myMnemonic: ""
+  };
+
+  navigateToNextPage = () => {
+    this.props.navigation.navigate(route.SPLASH_SCREEN);
+  };
+
+  saveMnemonic = async (value: any) => {
+    try {
+      await AsyncStorage.setItem("@MyStore:key", value);
+    } catch (error) {
+      console.log("Error occurs during saving data:::" + error);
+    }
   };
 }
