88eaee4f06dbb5166bfd9d00e5a10d2208c90d11
DianaLeee <yeonylee94@gmail.com>
2019-01-28 14:06:08 +0900

    ENH: 니모닉 백업 UI 추가

--- src/container/BackUpMnemonicScreen/Styles.ts
+++ src/container/BackUpMnemonicScreen/Styles.ts
@@ -6,5 +6,19 @@ export default StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+  createButton: {
+    width: 250,
+    height: 40,
+    backgroundColor: "#030066",
+    margin: 15
+  },
+  mnemonicContainer: {
+    flex: 0.3,
+    flexDirection: "column",
+    width: "90%",
+    height: "30%",
+    margin: 10,
+    backgroundColor: "#fff"
   }
 });
--- src/container/BackUpMnemonicScreen/index.tsx
+++ src/container/BackUpMnemonicScreen/index.tsx
@@ -1,27 +1,90 @@
 import React from "react";
-import { View } from "react-native";
+import { View, AsyncStorage } from "react-native";
 import { route } from "../../constants/route";
-import { Button, Text } from "react-native-paper";
+import {
+  Button,
+  Text,
+  Dialog,
+  Portal,
+  Paragraph,
+  Title,
+  TextInput
+} from "react-native-paper";
 import { NavigationScreenProps } from "react-navigation";
 import styles from "./Styles";
 
-export default class BackUpMnemScreen extends React.Component<NavigationScreenProps> {
+const ethers = require("ethers");
+
+export default class BackUpMnemonicScreen extends React.Component<
+  NavigationScreenProps
+> {
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is BackUp Mnem Screen</Text>
+        <Portal>
+          <Dialog visible={this.state.visible} onDismiss={this.hideDialog}>
+            <Dialog.Title>Mnemonic Backup</Dialog.Title>
+            <Dialog.Content>
+              <Paragraph>
+                Before using wallet, please backup your mnemonic
+              </Paragraph>
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button onPress={this.hideDialog}>Done</Button>
+            </Dialog.Actions>
+          </Dialog>
+        </Portal>
+        <Title>Mnemonic Backup</Title>
+        <Text>Please backup your Mnemonic before using the wallet</Text>
+        <TextInput
+          style={styles.mnemonicContainer}
+          mode={"outlined"}
+          multiline={true}
+          value={this.state.myMnemonic}
+        />
         <Button
-          icon="add-a-photo"
+          style={styles.createButton}
           mode="contained"
-          onPress={this.navigateToWallet}
+          onPress={this.navigateToNextPage}
         >
-          월렛으로
+          Done
         </Button>
       </View>
     );
   }
 
-  navigateToWallet = () => {
-    this.props.navigation.navigate(route.SUMMARY_SCREEN);
+  state = {
+    visible: true,
+    myMnemonic: ""
+  };
+
+  navigateToNextPage = () => {
+    this.props.navigation.navigate(route.SPLASH_SCREEN);
+  };
+
+  hideDialog = () => {
+    this.setState({ visible: false });
+    this.generateMnemonic();
+  };
+
+  generateMnemonic = async () => {
+    try {
+      const myMnemonic = ethers.Wallet.createRandom().mnemonic;
+      await AsyncStorage.setItem("@MyStore:key", myMnemonic);
+      this.getMnemonic();
+    } catch (error) {
+      console.log("Error occurs during saving data:::" + error);
+    }
+  };
+
+  getMnemonic = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@MyStore:key");
+      if (value !== null) {
+        this.setState({ myMnemonic: value });
+      }
+    } catch (error) {
+      console.log("Error occurs during retriving data:::" + error);
+    }
   };
 }
