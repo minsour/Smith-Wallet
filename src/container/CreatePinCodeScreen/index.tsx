b7dc0913d340c8c2eb5f3cecff773171906448ce
DianaLeee <yeonylee94@gmail.com>
2019-01-29 15:55:08 +0900

    CLN: 사용하지 않는 모듈 삭제

--- src/container/CreatePinCodeScreen/index.tsx
+++ src/container/CreatePinCodeScreen/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, AsyncStorage } from "react-native";
+import { View } from "react-native";
 import { NavigationScreenProps } from "react-navigation";
 import { route } from "../../constants/route";
 import styles from "./Styles";
