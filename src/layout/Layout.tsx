451f03dba9a2b894feb9260c9f51d1ac30ec24ce
DevSoopark <sparkm0128@gmail.com>
2019-01-30 18:34:27 +0900

    ENH: Layout 수정

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -5,10 +5,10 @@ import { UserHeader } from "../components/UserHeader";
 import styles from "./Styles";
 
 interface LayoutProps {
-    header: boolean  // 없으면, 헤더가 없는 레이아웃
+    header: boolean  // false면, 헤더가 없는 레이아웃
     headerTitle?: string;  // 없으면, 타이틀이 없는 헤더
     headerNavigation?: NavigationScreenProp<any,any>;  // 없으면, 뒤로가기 버튼이 없는 헤더
-    children: JSX.Element[];
+    children: JSX.Element[] | JSX.Element;
   }
 
 export class Layout extends React.Component<LayoutProps> {
