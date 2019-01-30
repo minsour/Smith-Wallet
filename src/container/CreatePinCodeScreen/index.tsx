65e411596f1b8ca903f895a815abb8afe1869c65
DevSoopark <sparkm0128@gmail.com>
2019-01-30 19:06:07 +0900

    DOC: 핀코드 스크린 문구 수정

--- src/container/CreatePinCodeScreen/index.tsx
+++ src/container/CreatePinCodeScreen/index.tsx
@@ -16,6 +16,9 @@ export class CreatePinCodeScreen extends React.Component<
           passwordLength={6}
           titleChoose="암 호"
           subtitleChoose="지갑 비밀번호를 생성해 주세요."
+          // node_modules/@haskkor/react-native-pincode/dist/index.js
+          // 에서 암호 다시 입력하는(2번째) 스크린의 문구 
+          // "지갑 비밀번호를 생성해 주세요." 로 수정
           storePin={AsyncStorageUtils.storePin}
         />
       </Layout>
