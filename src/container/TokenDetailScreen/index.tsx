e2f39345c094bcea4ebfa3f5a7819e245c3136be
DevSoopark <sparkm0128@gmail.com>
2019-01-30 18:59:34 +0900

    DOC: Layout 마저 적용

--- src/container/TokenDetailScreen/index.tsx
+++ src/container/TokenDetailScreen/index.tsx
@@ -1,18 +1,14 @@
 import React from "react";
-import { View } from "react-native";
 import { Text, Button } from "react-native-paper";
 import { NavigationScreenProps } from "react-navigation";
 import { styles } from "./Styles";
 import { route } from "../../constants/route";
-import { UserHeader } from "../../components/UserHeader";
-import UserStyle from "../../components/UserHeader/Styles";
+import { Layout } from '../../layout/Layout';
 
 export class TokenDetailScreen extends React.Component<NavigationScreenProps> {
   render() {
     return (
-      <View style={styles.container}>
-        <UserHeader title="토큰" />
-        <View style={UserStyle.userBody}>
+        <Layout header={true} headerTitle="토 큰">
           <Text>토큰 디테일 스크린</Text>
           <Button
             style={styles.Button}
@@ -28,8 +24,7 @@ export class TokenDetailScreen extends React.Component<NavigationScreenProps> {
           >
             Send 스크린
           </Button>
-        </View>
-      </View>
+        </Layout>
     );
   }
 
