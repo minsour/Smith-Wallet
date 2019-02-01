82fca0277750fd6388d6c2cfa787d46e55eb619f
DevSoopark <sparkm0128@gmail.com>
2019-02-01 09:46:20 +0900

    ENH: ListRoute 삭제

--- src/route/BitcoinRoute.tsx
+++ src/route/BitcoinRoute.tsx
@@ -1,11 +1,9 @@
 // src/route
 import { createSwitchNavigator } from "react-navigation";
 import { SummaryScreen } from "../container/SummaryScreen";
-import { ListRoute } from './ListRoute';
 
 export const BitcoinRoute = createSwitchNavigator(
     {
         Summary: SummaryScreen,
-        List: ListRoute
     }
 );
--- src/route/EthereumRoute.tsx
+++ src/route/EthereumRoute.tsx
@@ -1,11 +1,9 @@
 // src/route
 import { createSwitchNavigator } from "react-navigation";
 import { SummaryScreen } from "../container/SummaryScreen";
-import { ListRoute } from './ListRoute';
 
 export const EthereumRoute = createSwitchNavigator(
     {
         Summary: SummaryScreen,
-        List: ListRoute
     }
 );
