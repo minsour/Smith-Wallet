643f7b9fcee236d19e186a346324f10fd7c3d3db
DevSoopark <sparkm0128@gmail.com>
2019-02-05 00:12:55 +0900

    ENH: FAB.Group(메뉴)에 각각의 스크린 navigate 연결

--- src/container/MainScreen/index.tsx
+++ src/container/MainScreen/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { View } from "react-native";
 import { FAB, Provider as PaperProvider  } from "react-native-paper";
 import { NavigationScreenProps, createAppContainer } from "react-navigation";
 import { route } from "../../constants/route";
-import { Layout } from '../../layout/Layout';
 import { WalletRoute } from "../../route/WalletRoute";
-import { styles } from '../AuthorizePinCodeScreen/Styles';
 
 const WalletContainer = createAppContainer(WalletRoute);
 
@@ -13,46 +10,27 @@ export class MainScreen extends React.Component<NavigationScreenProps> {
   state = {
     open: false
   }
+
   render() {
     return (
       <PaperProvider>
         <WalletContainer/>
-      <FAB.Group
-        open={this.state.open}
-        icon={this.state.open ? 'close' : 'add'}
-        actions={[
-            { icon: 'settings', label: '설정', onPress: () => console.log('Pressed star')},
-            { icon: 'people', label: '친구', onPress: () => console.log('Pressed email') },
-            { icon: 'playlist-add', label: '추가', onPress: () => console.log('Pressed notifications') },
-        ]}
-        onStateChange={({ open }) => this.setState({ open })}
-        onPress={() => {
-            if (this.state.open) {
-            // do something if the speed dial is open
-            }
-            
-        }}
-      />
+        <FAB.Group
+          open={this.state.open}
+          icon={this.state.open ? 'close' : 'add'}
+          actions={[
+              { icon: 'settings', label: '설정', onPress: this.navigateToManageApp },
+              { icon: 'people', label: '친구', onPress: this.navigateToAddressList },
+              { icon: 'playlist-add', label: '추가', onPress: this.navigateToAddSomething },
+          ]}
+          onStateChange={({ open }) => this.setState({ open })}
+        />
       </PaperProvider>
     );
   }
-  
-  private navigateToSummaryTx = () => {
-    this.props.navigation.navigate(route.SUMMARY_TX_ROUTE);
-  };
 
-  private navigateToSummarySend = () => {
-    this.props.navigation.navigate(route.AUTHORIZE_PINCODE_SCREEN, {
-      destination: route.SELECT_ADDRESS_SCREEN
-    });
-  };
-
-  private navigateToTokenDetail = () => {
-    this.props.navigation.navigate(route.TOKEN_DETAIL_SCREEN);
-  };
-
-  private navigateToAddToken = () => {
-    this.props.navigation.navigate(route.ADD_TOKEN_SCREEN);
+  private navigateToAddSomething = () => {
+    this.props.navigation.navigate(route.ADD_SOMETHING_SCREEN);
   };
 
   private navigateToAddressList = () => {
