d274d8481418d8707273d7e600a3c7a4ff18a12c
DevSoopark <sparkm0128@gmail.com>
2019-02-07 10:08:39 +0900

    ENH: App.tsx에 store 적용

--- App.tsx
+++ App.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { createAppContainer } from "react-navigation"; // Version can be specified in package.json
 import { RootRoute } from "./src/route/index";
 import { Provider as PaperProvider } from "react-native-paper";
+import { Provider } from "mobx-react";
+import { WalletStore } from './src/stores/walletStore';
 
 const AppContainer = createAppContainer(RootRoute);
+const walletStore = new WalletStore();
 
 export default class App extends React.Component {
   render() {
     return (
       <PaperProvider>
+        <Provider walletStore={walletStore}>
         <AppContainer />
+        </Provider>
       </PaperProvider>
     );
   }
