d2120ef3cdaf975c5209ceb94122eca938b4ddea
DevSoopark <sparkm0128@gmail.com>
2019-02-07 11:07:00 +0900

    ENH: BackUpMnemonicScreen에 store 주입, 스타일 수정

--- src/container/BackUpMnemonicScreen/index.tsx
+++ src/container/BackUpMnemonicScreen/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button, Text, Title, TextInput } from "react-native-paper";
-import { NavigationScreenProps } from "react-navigation";
+import { NavigationScreenProp } from "react-navigation";
 import { observer } from "mobx-react/native";
 import { observable } from "mobx";
 import { styles } from "./Styles";
@@ -8,12 +8,21 @@ import { AsyncStorageUtils } from "../../utils/asyncStorageUtils";
 import { route } from "../../constants/route";
 import { Layout } from '../../layout/Layout';
 import 'ethers/dist/shims.js';  // 안드로이드 니모닉 생성 시 발생하는 오류 해결(String.prototype.normalize 사용)
+import { WalletStore } from '../../stores/walletStore';
+import { inject } from 'mobx-react';
+import { Loading } from '../../layout/Loading';
 
 const ethers = require("ethers");
 
+interface BackUpMnemonicScreenProps {
+  navigation: NavigationScreenProp<any,any>
+  walletStore: WalletStore
+}
+
+@inject("walletStore")
 @observer
 export class BackUpMnemonicScreen extends React.Component<
-  NavigationScreenProps
+  BackUpMnemonicScreenProps
 > {
   @observable myMnemonic: string = "";
 
@@ -22,6 +31,10 @@ export class BackUpMnemonicScreen extends React.Component<
   }
 
   render() {
+    if(!this.myMnemonic) {
+      // 제대로 작동 안하는 중 !!
+      return <Loading>지갑 생성중</Loading>
+    }
     return (
       <Layout header={true} headerTitle="Mnemonic 백업">
         <Title>Mnemonic Backup</Title>
@@ -37,6 +50,7 @@ export class BackUpMnemonicScreen extends React.Component<
           mode="contained"
           onPress={() => {
             this.saveMnemonic(this.myMnemonic);
+            this.setWallet();
             this.navigateToWallet();
           }}
         >
@@ -56,5 +70,18 @@ export class BackUpMnemonicScreen extends React.Component<
 
   private navigateToWallet = () => {
     this.props.navigation.navigate(route.MAIN_SCREEN);
+  };
+  
+  private setWallet = async () => {
+    await this.setMnemonic();
+    const { walletStore } = this.props;
+    const path = "m/44'/60'/0/0";
+    walletStore.setWallet(ethers.Wallet.fromMnemonic(walletStore.getMnemonic, path));
+  };
+
+  private setMnemonic = async () => {
+    const { walletStore } = this.props;
+    await AsyncStorageUtils.loadMnemonic()
+    .then( res => walletStore.setMnemonic(res) );
   };
 }
