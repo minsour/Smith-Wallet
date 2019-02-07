7f277100a2f26987e09cebfd106e1dc67f014217
DevSoopark <sparkm0128@gmail.com>
2019-02-07 16:52:23 +0900

    DOC: Dimentions width, height 분리

---
+++ src/constants/dementions.ts
@@ -0,0 +1,9 @@
+import { Dimensions } from 'react-native';
+
+const DIMENSIONS_WIDTH = Dimensions.get("window").width;
+const DIMENSIONS_HEIGHT = Dimensions.get("window").height;
+
+export {
+    DIMENSIONS_WIDTH,
+    DIMENSIONS_HEIGHT,
+}
