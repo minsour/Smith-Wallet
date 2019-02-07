89a4e377ecab0ed51f95b2e953599e040aab207c
DevSoopark <sparkm0128@gmail.com>
2019-02-07 11:07:32 +0900

    ENH: MainScreen에 store 주입, Loading 적용

--- src/container/MainScreen/index.tsx
+++ src/container/MainScreen/index.tsx
@@ -1,31 +1,49 @@
 import React from "react";
 import { FAB, Provider as PaperProvider  } from "react-native-paper";
-import { NavigationScreenProps, createAppContainer } from "react-navigation";
+import { NavigationScreenProp, createAppContainer } from "react-navigation";
 import { route } from "../../constants/route";
 import { WalletSummaryRoute } from "../../route/WalletSummaryRoute";
+import { inject, observer } from 'mobx-react';
+import { WalletStore } from '../../stores/walletStore';
+import { Loading } from '../../layout/Loading';
 
 const WalletSummaryContainer = createAppContainer(WalletSummaryRoute);
 
-export class MainScreen extends React.Component<NavigationScreenProps> {
+interface MainScreenProps {
+  navigation: NavigationScreenProp<any,any>
+  walletStore: WalletStore
+}
+
+@inject("walletStore")
+@observer
+export class MainScreen extends React.Component<MainScreenProps> {
   state = {
     open: false
   }
 
   render() {
+    const { walletStore } = this.props;
+    if(!walletStore.getWallet) {
+      return <Loading>지갑 로딩중</Loading>
+    }
     return (
-      <PaperProvider>
-        <WalletSummaryContainer/>
-        <FAB.Group
-          open={this.state.open}
-          icon={this.state.open ? 'close' : 'add'}
-          actions={[
-              { icon: 'settings', label: '설정', onPress: this.navigateToManageApp },
-              { icon: 'people', label: '친구', onPress: this.navigateToAddressList },
-              { icon: 'playlist-add', label: '추가', onPress: this.navigateToAddSomething },
-          ]}
-          onStateChange={({ open }) => this.setState({ open })}
-        />
-      </PaperProvider>
+        <PaperProvider>
+          <WalletSummaryContainer/>
+          <FAB.Group
+            // node_modules/react-native-paper/src/components/FAB/FABGroup.js
+            // 에서 282번째 줄 small 삭제하고, 하단의 스타일시트에서
+            // fab의 marginHorizontal: 24, marginBottom: 24, backgroundColor: "#fff"
+            // label의 height: 35, elevation: 7
+            open={this.state.open}
+            icon={this.state.open ? 'close' : 'add'}
+            actions={[
+                { icon: 'settings', label: '설정', onPress: this.navigateToManageApp },
+                { icon: 'people', label: '친구', onPress: this.navigateToAddressList },
+                { icon: 'playlist-add', label: '추가', onPress: this.navigateToAddSomething },
+            ]}
+            onStateChange={({ open }) => this.setState({ open })}
+          />
+        </PaperProvider> 
     );
   }
 
