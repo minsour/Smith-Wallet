b4a4039280b9ae06b42b2f4f1010b25fb8ba241e
DianaLeee <yeonylee94@gmail.com>
2019-02-08 16:38:43 +0900

    ENH: TokenDetail UI 업데이트

--- src/container/TokenDetailScreen/index.tsx
+++ src/container/TokenDetailScreen/index.tsx
@@ -1,40 +1,45 @@
-import React from "react";
-import { Text, Button } from "react-native-paper";
-import { NavigationScreenProps } from "react-navigation";
-import { styles } from "./Styles";
-import { route } from "../../constants/route";
+import React from 'react';
+import { View } from 'react-native';
+import { Text, Provider } from 'react-native-paper';
+import { NavigationScreenProp, createAppContainer } from 'react-navigation';
+import { styles } from './Styles';
+import { route } from '../../constants/route';
 import { Layout } from '../../layout/Layout';
+import { TxSummaryListHeader } from '../../route/TxSummaryListHeader';
 
-export class TokenDetailScreen extends React.Component<NavigationScreenProps> {
+const TxSummaryListContainer = createAppContainer(TxSummaryListHeader);
+
+interface TokenDetailScreenProps {
+  navigation: NavigationScreenProp<any, any>;
+}
+
+export class TokenDetailScreen extends React.Component<TokenDetailScreenProps> {
   render() {
     return (
-        <Layout header={true} headerTitle="토 큰" headerNavigation={this.props.navigation}>
-          <Text>토큰 디테일 스크린</Text>
-          <Button
-            style={styles.Button}
-            mode="contained"
-            onPress={this.navigateToDetailTx} // 테스트용
-          >
-            Receive 스크린
-          </Button>
-          <Button
-            style={styles.Button}
-            mode="contained"
-            onPress={this.navigateToSend} // 테스트용
-          >
-            Send 스크린
-          </Button>
-        </Layout>
+      <Layout header={false}>
+        <View style={styles.summary}>
+          <Text style={styles.summaryFont}>이더리움</Text>
+          <View style={styles.balance}>
+            <Text style={styles.balanceFont}>789,000</Text>
+            <Text style={styles.krwFont}>KRW</Text>
+          </View>
+          {/* <Text style={styles.addressFont}>{this.props.walletStore.getWallet.address}</Text> */}
+          <Text style={styles.addressFont}>address...</Text>
+        </View>
+        <Provider>
+          <TxSummaryListContainer />
+        </Provider>
+      </Layout>
     );
   }
 
-  private navigateToDetailTx = () => {
-    this.props.navigation.navigate(route.DETAIL_TX_ROUTE);
-  };
+  // private navigateToDetailTx = () => {
+  //   this.props.navigation.navigate(route.DETAIL_TX_ROUTE);
+  // };
 
-  private navigateToSend = () => {
-    this.props.navigation.navigate(route.AUTHORIZE_PINCODE_SCREEN, {
-      destination: route.SELECT_ADDRESS_SCREEN
-    });
-  };
+  // private navigateToSend = () => {
+  //   this.props.navigation.navigate(route.AUTHORIZE_PINCODE_SCREEN, {
+  //     destination: route.SELECT_ADDRESS_SCREEN,
+  //   });
+  // };
 }
