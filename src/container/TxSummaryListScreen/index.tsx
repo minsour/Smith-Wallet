245363c8f2829ec2a6e76e3e0838e0dafea767fe
DianaLeee <yeonylee94@gmail.com>
2019-02-11 14:12:41 +0900

    ENH: List.Section style 수정

--- src/container/TxSummaryListScreen/index.tsx
+++ src/container/TxSummaryListScreen/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
 import { NavigationScreenProps } from 'react-navigation';
 import { Layout } from '../../layout/Layout';
 import { List } from 'react-native-paper';
@@ -11,8 +9,7 @@ export class TxSummaryListScreen extends React.Component<
   render() {
     return (
       <Layout header={false}>
-        {/* <Text>트랜잭션 리스트</Text> */}
-        <List.Section>
+        <List.Section width="100%">
           <List.Item
             title="First Item"
             description="Item description"
