939ce14b63004f27d85d01f1f548f991928f6e07
DevSoopark <sparkm0128@gmail.com>
2019-02-14 02:37:55 +0900

    DOC: ERC20 관련 apiUrl 추가

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -1,5 +1,11 @@
 const UPBIT_MARKET_CODE_URL = 'https://api.upbit.com/v1/market/all'
+const ERC_SYMBOL_TO_CONTRACT = 'https://ethfaction.github.io/erc20_tokens/mainnet/symbol_to_contract'
+const ERC_SYMBOL_TO_DETAILS = 'https://ethfaction.github.io/erc20_tokens/mainnet/symbol_to_details'
+const ERC_CONTRACT_TO_ABI = 'https://ethfaction.github.io/erc20_tokens/mainnet/contract_to_abi'
 
 export {
     UPBIT_MARKET_CODE_URL,
+    ERC_SYMBOL_TO_CONTRACT,
+    ERC_SYMBOL_TO_DETAILS,
+    ERC_CONTRACT_TO_ABI
 }
