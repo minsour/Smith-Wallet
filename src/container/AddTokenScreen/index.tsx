7f4800940a100e7dc2fc0031e3086bfa85c83367
DevSoopark <sparkm0128@gmail.com>
2019-02-18 15:28:04 +0900

    ENH: AddTokenScreen UI 수정

--- src/container/AddTokenScreen/index.tsx
+++ src/container/AddTokenScreen/index.tsx
@@ -1,12 +1,49 @@
 import React from "react";
-import { Text } from "react-native-paper";
+import { Text, Searchbar, List } from "react-native-paper";
 import { Layout } from '../../layout/Layout';
+import { inject } from 'mobx-react';
+import { observer } from 'mobx-react/native';
+import { styles } from "./Styles";
+import { View, ScrollView } from 'react-native';
+import { NavigationScreenProp } from 'react-navigation';
+import { RootStore } from '../../stores';
+import { Token } from '../../components/Token';
+import { Symbol } from '../../components/Symbol';
 
-export class AddTokenScreen extends React.Component {
+interface AddTokenScreenProps {
+  rootStore: RootStore
+  navigation: NavigationScreenProp<any,any>
+}
+
+@inject('rootStore')
+@observer
+export class AddTokenScreen extends React.Component<AddTokenScreenProps> {
   render() {
+    const { tokenStore } = this.props.rootStore
+    let tokenId: number = 0
     return (
-      <Layout header={false}>
-          <Text>토큰 추가 스크린</Text>
+      <Layout header={true} headerTitle="토큰 추가" headerNavigation={this.props.navigation}>
+        <View style={styles.searchBarContainer}>
+          <Searchbar
+            placeholder="검색하기"
+            onChangeText={()=>{}}
+            value={""}
+            style={styles.searchBar}
+          />
+        </View>
+        <View style={styles.list}>
+          <ScrollView>
+            {tokenStore.searchedTokenList.map(token =>
+              <Token
+                key={`${tokenId++}`}
+                name={token.koreanName}
+                symbol={token.symbol}
+                address={token.address}
+                token={token}
+              />)
+            }
+          </ScrollView>
+        </View>
       </Layout>
     );
   }
