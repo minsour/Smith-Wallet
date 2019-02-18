39187432a4840bbb7626f88c13ece4d5b57ddb61
DevSoopark <sparkm0128@gmail.com>
2019-02-19 00:58:59 +0900

    ENH: TokenListScreen에 selectedTokenList 렌더링

--- src/container/TokenListScreen/index.tsx
+++ src/container/TokenListScreen/index.tsx
@@ -1,35 +1,38 @@
 import React from "react";
-import { Text, Button } from "react-native-paper";
 import { NavigationScreenProp } from "react-navigation";
 import { styles } from "./Styles";
 import { route } from "../../constants/route";
 import { Layout } from '../../layout/Layout';
+import { ScrollView } from 'react-native';
+import { RootStore } from '../../stores';
+import { inject, observer } from 'mobx-react';
+import { Token } from '../../components/Token';
 
 interface TokenListScreenProps {
-    navigation: NavigationScreenProp<any,any>
+  navigation: NavigationScreenProp<any, any>
+  rootStore: RootStore
 }
 
+@inject('rootStore')
+@observer
 export class TokenListScreen extends React.Component<TokenListScreenProps> {
   render() {
+    const { tokenStore } = this.props.rootStore
+    let tokenId: number = 0
     return (
       <Layout header={false}>
-        <Text>
-          토큰 리스트 스크린
-        </Text>
-        <Button
-          style={styles.Button}
-          mode="contained"
-          onPress={this.navigateToDetailTx} // 테스트용
-        >
-          추가
-        </Button>
-        <Button
-          style={styles.Button}
-          mode="contained"
-          onPress={this.navigateToSend} // 테스트용
-        >
-          송금
-        </Button>
+        <ScrollView>
+            {tokenStore.selectedTokenList.map(token =>
+              <Token
+                key={`${tokenId++}`}
+                balance={12300}
+                name={token.koreanName}
+                symbol={token.symbol}
+                address={token.address}
+                token={token}
+              />)
+            }
+          </ScrollView>
       </Layout>
     );
   }
