e5590ed4184798b227d3edcd5a9d07782937f457
DevSoopark <sparkm0128@gmail.com>
2019-02-18 15:32:33 +0900

    ENH: Etherscan의 ERC20 Top Token 파싱하도록 수정

--- src/apis/ERC20API.ts
+++ src/apis/ERC20API.ts
@@ -1,22 +1,58 @@
-import { GET } from '../constants/apiMethod';
-import {
-  ERC_CONTRACT_TO_ABI,
-  ERC_SYMBOL_TO_CONTRACT,
-  ERC_SYMBOL_TO_DETAILS
-} from '../constants/apiUrl';
+import { parse } from 'js-html-parser';
 
-const tokenMapping = (method: string, url: string) => {
-  return fetch(url, { method: method }).then((response) => response.json())
+interface EtherscanTopToken {
+  address: string
+  symbol: string
+  englishName: string
 }
 
-export const getERCAddress = () => {
-  return tokenMapping(GET, ERC_SYMBOL_TO_CONTRACT)
+const TopTokenList:EtherscanTopToken[] = []
+
+const requestEtherscanTopToken = (pageNumber: number) => {
+  return fetch(`https://etherscan.io/tokens?ps=100&p=${pageNumber}`)
 }
 
-export const getERCDetail = () => {
-  return tokenMapping(GET, ERC_SYMBOL_TO_DETAILS)
+const getAddress = (token: any) => {
+  let rawAttrs = JSON.stringify(token.rawAttrs)
+  const startIndex = rawAttrs.indexOf('0x')  //'0x' 부터 주소 시작
+  const endIndex = rawAttrs.length - 2
+  let address = rawAttrs.substring(startIndex, endIndex)
+  return address
 }
 
-export const getERCAbi = () => {
-  return tokenMapping(GET, ERC_CONTRACT_TO_ABI)
+const getSymbol = (token: any) => {
+  let rawText = JSON.stringify(token.rawText)
+  const startIndex: number = rawText.indexOf('(') + 1  //'(' 뒤에서 symbol 시작
+  const endIndex: number = rawText.length - 2
+  let symbol = rawText.substring(startIndex, endIndex)
+  return symbol
+}
+
+const getEnglishName = (token: any) => {
+  let rawText = JSON.stringify(token.rawText)
+  const startIndex: number = 1
+  const endIndex: number = rawText.indexOf('(') -  1  //'(' 앞에서 name 끝남
+  let englishName = rawText.substring(startIndex, endIndex)
+  return englishName.toUpperCase().replace(/ |TOKEN/gi , "")  //정규표현식으로 ' '랑 'TOKEN' 없애고 리턴
+}
+
+const parseEtherscanTopToken = (response: any) => {
+  const html = parse(response._bodyInit)
+  html.querySelectorAll('.text-primary').forEach(token => {
+    // class='text-primary' 인 html 태그에 Token 정보가 있음
+    TopTokenList.push({
+      address: getAddress(token),
+      symbol: getSymbol(token),
+      englishName: getEnglishName(token)
+    })
+  })
+}
+
+export const getEtherscanTopToken = async () => {
+  for (let pageNumber = 1; pageNumber < 10; pageNumber++) {
+    await requestEtherscanTopToken(pageNumber)
+      .then((response) => parseEtherscanTopToken(response))
+      .catch()
+  }
+  return TopTokenList
 }
