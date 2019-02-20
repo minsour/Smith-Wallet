265c9fb95f049a78f508332e63bca16338077316
DevSoopark <sparkm0128@gmail.com>
2019-02-20 14:52:23 +0900

    ENH: store 적용 방식 수정

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ export default class App extends React.Component {
   render() {
     return (
       <PaperProvider>
-        <Provider rootStore={rootStore} >
+        <Provider {...rootStore}>
         <AppContainer />
         </Provider>
       </PaperProvider>
