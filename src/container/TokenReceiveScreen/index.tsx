8752f5e2c295c26df4a1556c3d3a46b2e9976415
DianaLeee <yeonylee94@gmail.com>
2019-02-21 20:08:41 +0900

    BUG: QR코드 오류 수정

--- src/container/TokenReceiveScreen/index.tsx
+++ src/container/TokenReceiveScreen/index.tsx
@@ -4,7 +4,7 @@ import { NavigationScreenProp } from 'react-navigation';
 import { WalletStore } from '../../stores/walletStore';
 import { Layout } from '../../layout/Layout';
 import { inject, observer } from 'mobx-react';
-// import QRCode from 'react-native-qrcode';
+import QRCode from 'react-native-qrcode';
 
 interface TokenReceiveScreenProps {
   navigation: NavigationScreenProp<any, any>;
@@ -23,12 +23,13 @@ export class TokenReceiveScreen extends React.Component<
         headerTitle="입금 주소"
         headerNavigation={this.props.navigation}
       >
-        {/* <QRCode
-          value={this.props.walletStore.getWallet.address}
+        <QRCode
+          // value={this.props.walletStore.getWallet.address}
+          value="address(If I successfully load EOA, this appears without any errors)"
           size={200}
           bgColor="black"
           fgColor="white"
-        /> */}
+        />
         <Text>{this.props.walletStore.getWallet.address}</Text>
       </Layout>
     );
