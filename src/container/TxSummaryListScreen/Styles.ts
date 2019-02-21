c98be6650d445907a5e10db8ab075abe44a3ce19
DianaLeee <yeonylee94@gmail.com>
2019-02-21 20:10:20 +0900

    ENH: 리스트가 실제 ERC20 Transaction history를 나타내게 수정

---
+++ src/container/TxSummaryListScreen/Styles.ts
@@ -0,0 +1,54 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  //   listSectionContainer: {
+  //     width: '100%',
+  //     margin: 5,
+  //     marginLeft: -5,
+  //     // flexDirection: 'row',
+  //     // justifyContent: 'center',
+  //   },
+  textInputContainer: {
+    // flex: 0.5,
+    width: '100%',
+    flexDirection: 'column',
+  },
+  switchContainer: {
+    flexDirection: 'row',
+  },
+  switchLeftContainer: {
+    width: '80%',
+    alignSelf: 'flex-start',
+  },
+  switchRightContainer: {
+    width: '20%',
+    alignSelf: 'flex-end',
+  },
+  textInput: {
+    margin: 15,
+  },
+  bottomButton: {
+    // width: '100%',
+    // backgroundColor: '#030066',
+    // display: 'flex',
+    width: '100%',
+    height: 40,
+    backgroundColor: '#030066',
+    margin: 15,
+  },
+  buttonContainer: {
+    width: '100%',
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: 15,
+  },
+  summaryFont: {
+    fontSize: 20,
+    margin: 15,
+    marginBottom: -5,
+  },
+  feeSwitch: {
+    marginTop: 15,
+  },
+});
--- src/container/TxSummaryListScreen/index.tsx
+++ src/container/TxSummaryListScreen/index.tsx
@@ -1,47 +1,94 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 import { NavigationScreenProp } from 'react-navigation';
 import { Layout } from '../../layout/Layout';
 import { List } from 'react-native-paper';
 import { WalletStore } from '../../stores/walletStore';
 import { AsyncStorageUtils } from '../../utils/asyncStorageUtils';
 import { inject, observer } from 'mobx-react';
+import { TokenStore } from '../../stores/tokenStore';
+import { observable } from 'mobx';
+import { styles } from '../TokenSendScreen/Styles';
+const moment = require('moment');
+const ethers = require('ethers');
 
 interface TxSummaryListScreenProps {
   navigation: NavigationScreenProp<any, any>;
   walletStore: WalletStore;
+  tokenStore: TokenStore;
 }
 
+interface TokenHistory {
+  hash: string; //transaction hash
+  from: string;
+  to: string;
+  timeStamp: string;
+  contractAddress: string;
+  value: string;
+}
+var keyIndex: number = 0;
+
+// 임시로 하드코딩한 값
+var contractAddress = '0xCe5559F046d8C01192E15f55063906F8d1c14790';
+var userAddress = '0x6b59210ade46b62b25e82e95ab390a7ccadd4c3a';
+
+// 아이콘 이름 상수화
+const SEND_ICON = 'file-upload';
+const RECEIVE_ICON = 'file-download';
+
 @inject('walletStore')
+@inject('tokenStore')
 @observer
 export class TxSummaryListScreen extends React.Component<
   TxSummaryListScreenProps
 > {
   componentDidMount() {
-    // AsyncStorageUtils.getERC20Infos(
-    //   '0xc858df16fb030c529c8b43469c42f354f98a8d57', //for sample data
-    // );
-
-    AsyncStorageUtils.storeErc20Token();
+    AsyncStorageUtils.getErc20TokenHistory(contractAddress, userAddress).then(
+      (responseJson: TokenHistory | any) => {
+        this.props.tokenStore.tokenHistoryList = JSON.parse(responseJson);
+      },
+    );
   }
   render() {
     return (
       <Layout header={false}>
-        <List.Section width="100%">
-          <List.Item
-            title="0xc858df16fb030c529c8b43469c42f354f98a8d57"
-            description="01/10/2019 14:59:00"
-            left={() => <List.Icon icon="file-download" />}
-            right={() => <Text>-0.3 ETH</Text>}
-          />
-          <List.Item
-            title="0x2Cc08D64C38FA547ecEe4B15Ef8238A5912B2206"
-            description="01/13/2019 14:59:00"
-            left={() => <List.Icon icon="file-upload" />}
-            right={() => <Text>+4 ETH</Text>}
-          />
+        <List.Section style={styles.listSectionContainer}>
+          <ScrollView>
+            {this.props.tokenStore.tokenHistoryList.map(token => (
+              <List.Item
+                key={`${keyIndex++}`}
+                title={token.to}
+                description={this.convertTimestamp(token.timeStamp)}
+                left={() => (
+                  <List.Icon
+                    icon={this.classifySendReceive(token.from, userAddress)}
+                    color="#000000"
+                  />
+                )}
+                right={() => (
+                  <Text>
+                    {this.convertValue(token.value)} {token.tokenSymbol}
+                  </Text>
+                )}
+              />
+            ))}
+          </ScrollView>
         </List.Section>
       </Layout>
     );
   }
+  private convertTimestamp = (timeStamp: string) => {
+    var result = moment.unix(parseInt(timeStamp));
+    return result.format('YYYY-MM-DD HH:mm:ss');
+  };
+  private convertValue = (value: string) => {
+    return ethers.utils.formatEther(value);
+  };
+  private classifySendReceive = (from: string, myAddress: string) => {
+    if (from === myAddress) {
+      return SEND_ICON;
+    } else {
+      return RECEIVE_ICON;
+    }
+  };
 }
