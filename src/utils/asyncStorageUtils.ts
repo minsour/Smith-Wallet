9e24f5152dfee2a32f9a2b14fab4359b2cc53bdf
DianaLeee <yeonylee94@gmail.com>
2019-02-22 14:20:21 +0900

    DOC: asyncStorage 사용하지 않는 함수 다른 파일로 분리

--- src/utils/asyncStorageUtils.ts
+++ src/utils/asyncStorageUtils.ts
@@ -1,43 +1,7 @@
 import { AsyncStorage } from 'react-native';
 import { erc20Abi } from './erc20Abi';
-import { parse } from 'js-html-parser';
 
 const ethers = require('ethers');
-
-interface Token {
-  name: string;
-  symbol: string;
-  address: string;
-  totalBalance: number;
-}
-
-interface TokenHistory {
-  hash: string; //transaction hash
-  from: string;
-  to: string;
-  timeStamp: string;
-  contractAddress: string;
-  value: string;
-}
-
-// var tokenHistoryList: TokenHistory[] = [];
-// const parseErc20TokenHistory = (response: any) => {
-//   tokenHistoryList = JSON.parse(response);
-//   // tokenHistoryList.push({
-//   //   hash: response.hash, //transaction hash
-//   //   from: response.from,
-//   //   to: response.to,
-//   //   timeStamp: response.timeStamp,
-//   //   contractAddress: response.contractAddress,
-//   //   value: response.value,
-//   // });
-//   console.log('tokenHistoryList:::' + tokenHistoryList[0].timeStamp);
-//   // TopTokenList.push({
-//   //   address: getAddress(token),
-//   //   symbol: getSymbol(token),
-//   //   englishName: getEnglishName(token)
-//   // });
-// };
 
 export class AsyncStorageUtils {
   static storeMnemonic = async (newMnemonic: string) => {
@@ -72,86 +36,54 @@ export class AsyncStorageUtils {
     return '';
   };
 
-  static getERC20Infos = async (userAddress: string) => {
-    try {
-      const { Contract } = require('ethers');
-      const defaultProvider = new ethers.getDefaultProvider('mainnet');
-      //원래는 AsyncStorage에서 불러와야함.
-      //Contract Address
-      const tokenAddress = '0xCe5559F046d8C01192E15f55063906F8d1c14790';
-      const contract = new Contract(tokenAddress, erc20Abi, defaultProvider);
-
-      const erc20Name = await contract.name();
-      const erc20Symbol = await contract.symbol();
-      const balance = await contract.balanceOf(userAddress);
-
-      var token: Token = {
-        name: erc20Name,
-        symbol: erc20Symbol,
-        address: tokenAddress,
-        totalBalance: ethers.utils.formatEther(balance),
-      };
-
-      return JSON.stringify(token);
-    } catch (error) {
-      console.error(
-        'Error occurs during getting ERC20 token information :::' + error,
-      );
-    }
-  };
-
-  /** 특정 EOA의 모든 거래 내역을 가지고 옴 **/
-  // static getTxHistoryByAddress = async (address: string) => {
+  // static getERC20Infos = async (userAddress: string) => {
   //   try {
-  //     const etherscanProvider = new ethers.providers.EtherscanProvider();
-  //     etherscanProvider.getHistory(address).then(function(history: string) {
-  //       console.log('FROM getTxHistoryByAddress' + history);
-  //       //parsing 작업 추가 필요
-  //       // 어케나누냐고요
-  //     });
+  //     const { Contract } = require('ethers');
+  //     const defaultProvider = new ethers.getDefaultProvider('mainnet');
+
+  //     const tokenAddress = '0xCe5559F046d8C01192E15f55063906F8d1c14790'; //contract address
+  //     const contract = new Contract(tokenAddress, erc20Abi, defaultProvider);
+
+  //     const erc20Name = await contract.name();
+  //     const erc20Symbol = await contract.symbol();
+  //     const balance = await contract.balanceOf(userAddress);
+
+  //     var tmpToken = {
+  //       name: erc20Name,
+  //       symbol: erc20Symbol,
+  //       address: tokenAddress,
+  //       totalBalance: ethers.utils.formatEther(balance),
+  //     };
+
+  //     return JSON.stringify(tmpToken);
   //   } catch (error) {
   //     console.error(
-  //       'Error occurs during loading transaction history from etherscanProvider:::' +
-  //         error,
+  //       'Error occurs during getting ERC20 token information :::' + error,
   //     );
   //   }
   // };
-  static getErc20TokenHistory = async (
-    contractAddress: string,
-    userAddress: string,
-  ) => {
-    try {
-      const ERC_TX_HISTORY =
-        'https://api.etherscan.io/api?module=account&action=tokentx&page=1&offset=100&sort=desc&';
-      const API_KEY = 'QYZRBUMFTPA75YXE3P8IWUQS8Q23R6875Y';
-      const finalApiUrl =
-        ERC_TX_HISTORY +
-        'contractaddress=' +
-        contractAddress +
-        '&address=' +
-        userAddress +
-        '&apiKey=' +
-        API_KEY;
-      // await fetch(finalApiUrl, { method: 'GET' }).then((response: any) => {});
-      let response = await fetch(finalApiUrl);
-      let responseJson = await response.json();
-      return JSON.stringify(responseJson.result); //responseJson.result; // object
-    } catch (error) {
-      console.error('Error during loading ERC20 TX history:::' + error);
-    }
-  };
 
-  // static getEthereumBalance = async (address: string) => {  //Detail 페이지에는 필요 없음
+  // static getErc20TokenHistory = async (
+  //   contractAddress: string,
+  //   userAddress: string,
+  // ) => {
   //   try {
-  //     const etherscanProvider = new ethers.providers.EtherscanProvider();
-  //     await etherscanProvider.getBalance(address).then((balance: string) => {
-  //       let etherString = ethers.utils.formatEther(balance);
-  //       console.log('Ether Balance: ' + etherString);
-  //     });
+  //     const ERC_TX_HISTORY =
+  //       'https://api.etherscan.io/api?module=account&action=tokentx&page=1&offset=100&sort=desc&';
+  //     const API_KEY = 'QYZRBUMFTPA75YXE3P8IWUQS8Q23R6875Y';
+  //     const finalApiUrl =
+  //       ERC_TX_HISTORY +
+  //       'contractaddress=' +
+  //       contractAddress +
+  //       '&address=' +
+  //       userAddress +
+  //       '&apiKey=' +
+  //       API_KEY;
+  //     let response = await fetch(finalApiUrl);
+  //     let responseJson = await response.json();
+  //     return JSON.stringify(responseJson.result);
   //   } catch (error) {
-  //     console.error(
-  //       'Error occurs during getting ethereum balance :::' + error,
-  //     );
+  //     console.error('Error during loading ERC20 TX history:::' + error);
   //   }
   // };
 }
