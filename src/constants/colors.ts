70ff7c492fb30e471dc071e0483e717027c6b06e
DianaLeee <yeonylee94@gmail.com>
2019-02-22 10:18:19 +0900

    ENH: 색상 상수화

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,5 +1,5 @@
-const BG_COLOR: string = "#f8f9ff";
+const BG_COLOR: string = '#f8f9ff';
+const SEND_ICON_COLOR: string = '#E84B39';
+const RECEIVE_ICON_COLOR: string = '#3096C9';
 
-export {
-    BG_COLOR,
-}
+export { BG_COLOR, SEND_ICON_COLOR, RECEIVE_ICON_COLOR };
