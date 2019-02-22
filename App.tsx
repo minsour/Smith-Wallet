817e603462d95527134a30ab8b42c0a3c0b7c9be
DianaLeee <yeonylee94@gmail.com>
2019-02-22 11:20:15 +0900

    ENH: mobX스토어 추가

--- App.tsx
+++ App.tsx
@@ -4,17 +4,17 @@ import { RootRoute } from './src/route/index';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Provider } from 'mobx-react';
 import { WalletStore } from './src/stores/walletStore';
-import { TokenStore } from './src/stores/tokenStore';
+import { TokenInfoStore } from './src/stores/tokenInfoStore';
 
 const AppContainer = createAppContainer(RootRoute);
 const walletStore = new WalletStore();
-const tokenStore = new TokenStore();
+const tokenInfoStore = new TokenInfoStore();
 
 export default class App extends React.Component {
   render() {
     return (
       <PaperProvider>
-        <Provider walletStore={walletStore} tokenStore={tokenStore}>
+        <Provider walletStore={walletStore} tokenInfoStore={tokenInfoStore}>
           <AppContainer />
         </Provider>
       </PaperProvider>
