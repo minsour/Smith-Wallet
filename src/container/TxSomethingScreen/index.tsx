a24d365a655751b7902380ed79e7ee11d2132df5
DevSoopark <sparkm0128@gmail.com>
2019-02-24 12:10:38 +0900

    ENH: TxSomethingScree(모달) 추가

---
+++ src/container/TxSomethingScreen/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Text, TouchableRipple } from "react-native-paper";
+import { NavigationScreenProp } from 'react-navigation';
+import { View } from 'react-native';
+import { styles } from './Styles'
+import { route } from '../../constants/route';
+import { inject } from 'mobx-react';
+import { observer } from 'mobx-react/native';
+import { modal } from '../../constants/modal';
+import { ModalStore } from '../../stores/modalStore';
+import { store } from '../../constants/store';
+
+interface TxSomethingScreenProps {
+  navigation: NavigationScreenProp<any, any>
+  modalStore?: ModalStore
+}
+
+@inject(store.MODAL_STORE)
+@observer
+export class TxSomethingScreen extends React.Component<TxSomethingScreenProps> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableRipple style={styles.content} onPress={this.navigateToTokenReceive}>
+          <Text>
+            받기
+          </Text>
+        </TouchableRipple>
+        <TouchableRipple style={styles.content} onPress={this.navigateToTokenSend}>
+          <Text>
+            보내기
+          </Text>
+        </TouchableRipple>
+      </View>
+    );
+  }
+
+  private navigateToTokenReceive = () => {
+    this.props.modalStore!.hideModal(modal.TX_MODAL)
+    this.props.navigation.navigate(route.TOKEN_SEND_SCREEN)
+  }
+  private navigateToTokenSend = () => {
+    this.props.modalStore!.hideModal(modal.TX_MODAL)
+    this.props.navigation.navigate(route.TOKEN_SEND_SCREEN)
+  }
+}
