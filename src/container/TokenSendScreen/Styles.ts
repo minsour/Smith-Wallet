d9d552295d5b361728be35546af8d85bb4b93863
DevSoopark <sparkm0128@gmail.com>
2019-02-27 20:16:47 +0900

    ENH: TokenSendScreen 수정

--- src/container/TokenSendScreen/Styles.ts
+++ src/container/TokenSendScreen/Styles.ts
@@ -1,16 +1,177 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
+import { DIMENSIONS_WIDTH } from '../../constants/dementions';
 
 export const styles = StyleSheet.create({
+  // container: {
+  //   flexDirection: 'row',
+  //   justifyContent: 'center',
+  // },
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    // flex: 0.5,
+    width: DIMENSIONS_WIDTH,
+    alignItems: 'center'
   },
-  Button: {
-    width:250,
-    height:40,
-    backgroundColor:"#030066",
-    margin:15
+  bodyContainer: {
+    flex: 5,
+    width: DIMENSIONS_WIDTH,
+    alignItems: 'center'
+  },
+  inputContainer: {
+    width: DIMENSIONS_WIDTH - 40,
+    height: 180,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    borderColor: "#eaeaea",
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInputContainer: {
+    width: DIMENSIONS_WIDTH - 100
+  },
+  bottomContainer: {
+    flex: 3,
+    width: DIMENSIONS_WIDTH,
+    alignItems: 'center',
+  },
+  amountInput: {
+    margin: 15,
+    backgroundColor: "#fff",
+    width: DIMENSIONS_WIDTH - 80,
+  },
+  accountInput: {
+    margin: 15,
+    backgroundColor: "#fff",
+    width: DIMENSIONS_WIDTH - 80,
+    marginBottom: -3,
+    marginTop: -5
+  },
+  button: {
+    backgroundColor: "#030066",
+    borderRadius: 0,
+    width: DIMENSIONS_WIDTH,
+    height: 55,
+    marginTop: -20,
+    marginBottom:-3,
+    justifyContent: 'center'
+  },
+  buttonFont: {
+    color: "#fff",
+    fontSize: 17,
+  },
+  notButton: {
+    backgroundColor: "#d5d5d5",
+    borderRadius: 0,
+    width: DIMENSIONS_WIDTH,
+    height: 55,
+    marginTop: -20,
+    marginBottom:-3,
+    justifyContent: 'center'
+  },
+  notButtonFont: {
+    fontSize: 17,
+    color: "#626262"
+  },
+  label: {
+    fontSize: 14,
+    color: "#a6a6a6",
+    marginLeft: 12,
+    marginBottom:-13
+  },
+  summaryFont: {
+    fontSize: 20,
+    margin: 15,
+    marginBottom: -5,
+  },
+  feeSwitch: {
+    marginTop: 15,
+  },
+  subscription: {
+    marginTop: 18,
+    width: DIMENSIONS_WIDTH - 60,
+    borderBottomColor: "#4c4c4c",
+    borderBottomWidth: 1,
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  total: {
+    flexDirection: 'row',
+  },
+  totalText: {
+    fontSize: 14,
+    width: 100,
+    marginRight: DIMENSIONS_WIDTH - 360
+  },
+  totalNumber: {
+    fontSize: 14,
+    width: 150,
+    textAlign: 'right',
+    paddingRight: 5
+  },
+  totalSymbol: {
+    fontSize: 14,
+    width: 40,
+    color: "#4c4c4c"
+  },
+  transfer: {
+    flexDirection: 'row',
+    marginTop: 4,
+  },
+  transferText: {
+    fontSize: 14,
+    width: 100,
+    marginRight: DIMENSIONS_WIDTH - 360
+  },
+  transferNumber: {
+    fontSize: 14,
+    width: 150,
+    textAlign: 'right',
+    paddingRight: 5
+  },
+  transferSymbol: {
+    fontSize: 14,
+    width: 40,
+    color: "#4c4c4c"
+  },
+  gasfee: {
+    flexDirection: 'row',
+    marginTop: 4,
+  },
+  gasfeeText: {
+    fontSize: 14,
+    width: 100,
+    marginRight: DIMENSIONS_WIDTH - 360
+  },
+  gasfeeNumber: {
+    fontSize: 14,
+    width: 150,
+    textAlign: 'right',
+    paddingRight: 5
+  },
+  gasfeeSymbol: {
+    fontSize: 14,
+    width: 40,
+    color: "#4c4c4c"
+  },
+  balance: {
+    flexDirection: 'row',
+    marginTop: 4,
+    marginBottom: 12
+  },
+  balanceText: {
+    fontSize: 14,
+    width: 100,
+    marginRight: DIMENSIONS_WIDTH - 360
+  },
+  balanceNumber: {
+    fontSize: 14,
+    width: 150,
+    textAlign: 'right',
+    paddingRight: 5
+  },
+  balanceSymbol: {
+    fontSize: 14,
+    width: 40,
+    color: "#4c4c4c"
   }
 });
--- src/container/TokenSendScreen/index.tsx
+++ src/container/TokenSendScreen/index.tsx
@@ -1,16 +1,116 @@
-import React from "react";
-import { Text } from "react-native-paper";
-import { NavigationScreenProps } from "react-navigation";
+import React from 'react';
+import { View } from 'react-native';
+import { Text, Button, TextInput } from 'react-native-paper';
+import { NavigationScreenProp } from 'react-navigation';
 import { Layout } from '../../layout/Layout';
+import { styles } from './Styles';
+import { transferEthereum, getAccountInfo } from '../../apis/ethers';
+import { inject, observer } from 'mobx-react';
+import { store } from '../../constants/store';
+import { WalletStore } from '../../stores/walletStore';
+import { observable, action } from 'mobx';
+import { TokenStore } from '../../stores/tokenStore';
+import { ethers } from 'ethers';
 
-export class TokenSendScreen extends React.Component<NavigationScreenProps> {
+interface TokenSendScreenProps {
+  navigation: NavigationScreenProp<any, any>
+  walletStore?: WalletStore
+  tokenStore?: TokenStore
+}
+
+@inject(store.WALLET_STORE, store.TOKEN_STORE)
+@observer
+export class TokenSendScreen extends React.Component<TokenSendScreenProps> {
+  @observable amount = ''
+  @observable account = ''
+  title = this.props.tokenStore!.clickedToken.koreanName + ' 보내기'
   render() {
     return (
-      <Layout header={true} headerTitle="출 금" headerNavigation={this.props.navigation}>
-        <Text>
-        Send 스크린
-        </Text>
+      <Layout
+        header={true}
+        headerTitle={this.title}
+        headerNavigation={this.props.navigation}
+      >
+        <View style={styles.bodyContainer}>
+          <View style={styles.inputContainer}>
+            <View>
+              <TextInput
+                style={styles.accountInput}
+                label="받는 주소"
+                value={this.account}
+                underlineColor="#030066"
+                onChangeText={account => this.changeAccount(account)}
+              />
+            </View>
+            <View>
+              <TextInput
+                style={styles.amountInput}
+                label="송금 수량"
+                value={this.amount}
+                underlineColor="#030066"
+                onChangeText={amount => this.changeAmount(amount)}
+              />
+            </View>
+          </View>
+          <View style={styles.subscription}>
+            <View style={styles.total}>
+              <Text style={styles.totalText}>총액</Text>
+              <Text style={styles.totalNumber}>{this.props.tokenStore!.clickedToken.balance}</Text>
+              <Text style={styles.totalSymbol}>{this.props.tokenStore!.clickedToken.symbol}</Text>
+            </View>
+            <View style={styles.transfer}>
+              <Text style={styles.transferText}>송금 수량</Text>
+              <Text style={styles.transferNumber}>{this.amount}</Text>
+              <Text style={styles.transferSymbol}>{this.props.tokenStore!.clickedToken.symbol}</Text>
+            </View>
+            <View style={styles.gasfee}>
+              <Text style={styles.gasfeeText}>송금 수수료</Text>
+              <Text style={styles.gasfeeNumber}>{this.props.tokenStore!.clickedToken.gasfee}</Text>
+              <Text style={styles.gasfeeSymbol}>{this.props.tokenStore!.clickedToken.symbol}</Text>
+            </View>
+            <View style={styles.balance}>
+              <Text style={styles.balanceText}>이후 잔액</Text>
+              <Text style={styles.balanceNumber}>{this.amount}</Text>
+              <Text style={styles.balanceSymbol}>{this.props.tokenStore!.clickedToken.symbol}</Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.bottomContainer}>
+        </View>
+        {this.amount.length != 0 ?
+          <Button
+            style={styles.button}
+            onPress={this.transfer}
+          >
+            <Text style={styles.buttonFont}>
+              보내기
+            </Text>
+          </Button> :
+          <Button
+            style={styles.notButton}
+          >
+            <Text style={styles.notButtonFont}>
+              보내기
+            </Text>
+          </Button>
+        }
       </Layout>
     );
+  }
+
+  @action private changeAccount = (account: string) => {
+    this.account = account
+  }
+
+  @action private changeAmount = (amount: string) => {
+    this.amount = amount
+  }
+
+  private transfer = () => {
+    transferEthereum(getAccountInfo(this.props.walletStore!.getMnemonic, 0).privateKey,
+      this.account,
+       Number.parseFloat(this.amount))
+    .then((tx:any)=>{console.log(tx)})
+    .catch((tx:any)=>{console.log(tx)});
   }
 }
