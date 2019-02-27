5803a0fe699636f6bfc36dfdb98d5ea0d7a0e808
DevSoopark <sparkm0128@gmail.com>
2019-02-27 20:15:33 +0900

    DOC: 업비트 현재가 API url 추가

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -3,8 +3,10 @@ const UPBIT_MARKET_CODE_URL = 'https://api.upbit.com/v1/market/all'
 // const ERC_SYMBOL_TO_DETAILS = 'https://ethfaction.github.io/erc20_tokens/mainnet/symbol_to_details'
 // const ERC_CONTRACT_TO_ABI = 'https://ethfaction.github.io/erc20_tokens/mainnet/contract_to_abi'
 const GET_UPBIT_ERC_URL = 'http://icpc.dongguk.ac.kr:3000/v1/market/all'
+const UPBIT_GET_TICKER_URL = 'https://api.upbit.com/v1/ticker?markets='
 
 export {
     UPBIT_MARKET_CODE_URL,
     GET_UPBIT_ERC_URL,
+    UPBIT_GET_TICKER_URL,
 }
