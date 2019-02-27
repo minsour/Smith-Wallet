95de3633f131b740edab89d5dc9f203edcef4ac8
DevSoopark <sparkm0128@gmail.com>
2019-02-27 20:11:11 +0900

    ENH: 안쓰는 route 삭제

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -3,7 +3,6 @@ import { createSwitchNavigator } from "react-navigation";
 import { SplashScreen } from "../container/SplashScreen";
 import { LoginRoute } from "./LoginRoute";
 import { AuthorizePinCodeScreen } from "../container/AuthorizePinCodeScreen";
-import { MainScreen } from '../container/MainScreen';
 import { WalletRoute } from './WalletRoute';
 
 export const RootRoute = createSwitchNavigator({
@@ -12,5 +11,4 @@ export const RootRoute = createSwitchNavigator({
   AuthorizePinCode: AuthorizePinCodeScreen,
   Login: LoginRoute,
   Wallet: WalletRoute,
-  Main: MainScreen
 });
