5e093debcd803891d3bba9af68e0b1a558fd9d80
dianalee <diana.lee@dianaleeui-MacBookPro.local>
2019-03-04 03:33:27 +0900

    ENH: pincode 확인 후 route 분기에 따른 로직 추가

--- src/container/AuthorizePinCodeScreen/index.tsx
+++ src/container/AuthorizePinCodeScreen/index.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import { NavigationScreenProps } from 'react-navigation';
+import { NavigationScreenProp } from 'react-navigation';
 import { Layout } from '../../layout/Layout';
 import { AsyncStorageUtils } from '../../utils/asyncStorageUtils';
 import PINCode from '@haskkor/react-native-pincode';
+import { route } from '../../constants/route';
+import { WalletStore } from '../../stores/walletStore';
+import { inject } from 'mobx-react';
+import { store } from '../../constants/store';
+import { observer } from 'mobx-react/native';
+import { TokenStore } from '../../stores/tokenStore';
 
+interface AuthorizePinCodeScreenProps {
+  navigation: NavigationScreenProp<any, any>;
+  walletStore?: WalletStore;
+  tokenStore?: TokenStore;
+}
+
+@inject(store.WALLET_STORE)
+@inject(store.TOKEN_STORE)
+@observer
 export class AuthorizePinCodeScreen extends React.Component<
-  NavigationScreenProps
+  AuthorizePinCodeScreenProps
 > {
   state = {
     loadedPin: '',
+    destination: '',
   };
   componentDidMount() {
     this.verifyPincode();
+    this.setMnemonic();
   }
   render() {
     // 처음 핀코드 입력받는 창에서는 헤더 없고, 이후에는 뒤로가기 버튼은 있음
@@ -38,16 +55,27 @@ export class AuthorizePinCodeScreen extends React.Component<
       } else {
         //핀넘버 불러왔는데 핀넘버 있음 == 인증하기
         //인증후 요청화면으로 이동
+        //니모닉 있으면 메인화면으로 가기
         console.log('else: ' + pinCode);
         this.setState({ loadedPin: pinCode });
       }
     });
   };
 
-  private navigateToDestination = () => {
-    // 핀코드 입력 후에는 params로 받아온 목적지로 이동
-    this.props.navigation.navigate(
-      this.props.navigation.getParam('destination'),
+  private setMnemonic = async () => {
+    await AsyncStorageUtils.loadMnemonic().then(res =>
+      this.props.walletStore!.setMnemonic(res),
     );
+  };
+
+  private navigateToDestination = async () => {
+    await this.props.tokenStore!.loadTokenList();
+    if (this.props.walletStore!.getMnemonic !== null) {
+      console.log('지갑 있음');
+      this.props.navigation.navigate(route.MAIN_SCREEN);
+    } else {
+      console.log('지갑 없음');
+      this.props.navigation.navigate(route.INITIAL_SCREEN);
+    }
   };
 }
