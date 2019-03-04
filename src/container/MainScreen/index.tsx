54c23b239e3674fd848ad7d68587a2b8695556e5
dianalee <>
2019-03-04 18:20:14 +0900

    CLN: 핀코드 입력 횟수 조정

--- src/container/MainScreen/index.tsx
+++ src/container/MainScreen/index.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { FAB, Provider as PaperProvider  } from "react-native-paper";
-import { NavigationScreenProp, createAppContainer } from "react-navigation";
-import { route } from "../../constants/route";
-import { WalletSummaryRoute } from "../../route/WalletSummaryRoute";
+import React from 'react';
+import { FAB, Provider as PaperProvider } from 'react-native-paper';
+import { NavigationScreenProp, createAppContainer } from 'react-navigation';
+import { route } from '../../constants/route';
+import { WalletSummaryRoute } from '../../route/WalletSummaryRoute';
 import { inject, observer } from 'mobx-react';
 import { Loading } from '../../layout/Loading';
 import { styles } from './Styles';
 import { ModalLayout } from '../../layout/ModalLayout';
-import { modal } from '../../constants/modal'
+import { modal } from '../../constants/modal';
 import { AddSomethingScreen } from '../AddSomethingScreen';
 import { WalletStore } from '../../stores/walletStore';
 import { TokenStore } from '../../stores/tokenStore';
@@ -18,60 +18,78 @@ import { TxSomethingScreen } from '../TxSomethingScreen';
 const WalletSummaryContainer = createAppContainer(WalletSummaryRoute);
 
 interface MainScreenProps {
-  navigation: NavigationScreenProp<any,any>
-  walletStore?: WalletStore
-  tokenStore?: TokenStore
-  modalStore?: ModalStore
+  navigation: NavigationScreenProp<any, any>;
+  walletStore?: WalletStore;
+  tokenStore?: TokenStore;
+  modalStore?: ModalStore;
 }
 
 @inject(store.TOKEN_STORE, store.WALLET_STORE, store.MODAL_STORE)
 @observer
 export class MainScreen extends React.Component<MainScreenProps> {
   state = {
-    open: false
-  }
+    open: false,
+  };
 
   render() {
-    this.props.tokenStore!.initWillBeAddedToken()
-    if(this.props.walletStore!.getMnemonic === "") {
-      return <Loading>지갑 로딩중</Loading>
-    }
-    if (this.props.modalStore!.visible[modal.LOADING]) {
-      return <Loading>잔액 확인중</Loading>
-    }
+    this.props.tokenStore!.initWillBeAddedToken();
+    // if(this.props.walletStore!.getMnemonic === "") {
+    //   return <Loading>지갑 로딩중</Loading>
+    // }
+    // if (this.props.modalStore!.visible[modal.LOADING]) {
+    //   return <Loading>잔액 확인중</Loading>
+    // }
     return (
       <PaperProvider>
-        <WalletSummaryContainer/>
+        <WalletSummaryContainer />
         <FAB.Group
           open={this.state.open}
           icon={this.state.open ? 'close' : 'add'}
           actions={[
-            { style: styles.actionsStyle, icon: 'settings', label: '설정', onPress: this.navigateToManageApp },
-            { style: styles.actionsStyle, icon: 'people', label: '친구', onPress: this.navigateToAddressList },
-            { style: styles.actionsStyle, icon: 'playlist-add', label: '추가', onPress: this.showAddModal },
+            {
+              style: styles.actionsStyle,
+              icon: 'settings',
+              label: '설정',
+              onPress: this.navigateToManageApp,
+            },
+            {
+              style: styles.actionsStyle,
+              icon: 'people',
+              label: '친구',
+              onPress: this.navigateToAddressList,
+            },
+            {
+              style: styles.actionsStyle,
+              icon: 'playlist-add',
+              label: '추가',
+              onPress: this.showAddModal,
+            },
           ]}
           onStateChange={({ open }) => this.setState({ open })}
           fabStyle={styles.fabStyle}
         />
-        {this.props.modalStore!.visible[modal.ADD_MODAL] &&
-          <ModalLayout visibleKey={modal.ADD_MODAL} animationIn='slideInUp' animationOut='slideInDown'>
-            <AddSomethingScreen navigation={this.props.navigation}/>
+        {this.props.modalStore!.visible[modal.ADD_MODAL] && (
+          <ModalLayout
+            visibleKey={modal.ADD_MODAL}
+            animationIn="slideInUp"
+            animationOut="slideInDown"
+          >
+            <AddSomethingScreen navigation={this.props.navigation} />
           </ModalLayout>
-        }
+        )}
         {this.props.modalStore!.visible[modal.CLICK_TOKEN] &&
-          this.props.navigation.navigate(route.TOKEN_DETAIL_SCREEN)
-        }
-        {this.props.modalStore!.visible[modal.TX_MODAL] &&
+          this.props.navigation.navigate(route.TOKEN_DETAIL_SCREEN)}
+        {this.props.modalStore!.visible[modal.TX_MODAL] && (
           <ModalLayout visibleKey={modal.TX_MODAL}>
-            <TxSomethingScreen navigation={this.props.navigation}/>
+            <TxSomethingScreen navigation={this.props.navigation} />
           </ModalLayout>
-        }
+        )}
       </PaperProvider>
     );
   }
 
   private showAddModal = () => {
-    this.props.modalStore!.showModal(modal.ADD_MODAL)
+    this.props.modalStore!.showModal(modal.ADD_MODAL);
   };
 
   private navigateToAddressList = () => {
