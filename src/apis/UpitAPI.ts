97f69ea73a56bd5d0b68e608678deb8076e51eb4
DevSoopark <sparkm0128@gmail.com>
2019-03-06 18:33:15 +0900

    ENH: UPbit Private API 추가

--- src/apis/UpitAPI.ts
+++ src/apis/UpitAPI.ts
@@ -1,5 +1,6 @@
-import { UPBIT_MARKET_CODE_URL, GET_UPBIT_ERC_URL, UPBIT_GET_TICKER_URL } from '../constants/apiUrl';
+import { GET_UPBIT_ERC_URL, UPBIT_GET_TICKER_URL, UPBIT_GET_ACCOUNT_URL } from '../constants/apiUrl';
 import { GET } from '../constants/apiMethod'
+const jwt = require('react-native-jwt-io')
 
 const publicAPI = (method: string, url: string, body: {} = {}) => {
   // fetch는 Promise를 반환하므로, 받아서 바로 .then() 쓰면 됨.
@@ -15,12 +16,19 @@ const publicAPI = (method: string, url: string, body: {} = {}) => {
   }
 }
 
-const privateAPI = () => {
-  // 향후 구현 예정
-}
+const privateAPI = (method: string, url: string, accessKey:string, secretKey:string) => {
+  const payload = {
+    access_key: accessKey,
+    nonce: (new Date).getTime()
+  }
+  const token = jwt.encode(payload, secretKey);
 
-export const getMarketCode = () => {
-  return publicAPI(GET, UPBIT_MARKET_CODE_URL)
+  var options = {
+    method: method,
+    headers: {Authorization: `Bearer ${token}`}
+  };
+
+  return fetch(url, options).then((response) => response.json())
 }
 
 export const getERCToken = () => {
@@ -29,4 +37,8 @@ export const getERCToken = () => {
 
 export const getTokenTicker = (marketCode:string) => {
   return publicAPI(GET, `${UPBIT_GET_TICKER_URL}${marketCode}`)
+}
+
+export const getUpbitAccount = (accessKey:string, secretKey:string) => {
+  return privateAPI(GET, UPBIT_GET_ACCOUNT_URL, accessKey, secretKey)
 }
