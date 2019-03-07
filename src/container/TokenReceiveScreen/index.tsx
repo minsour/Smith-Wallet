c2d251d467b1c0f6746d3fee74013d19bae7587a
dianalee <>
2019-03-07 09:16:41 +0900

    ENH: 올바른 EOA를 가져오도록 수정

--- src/container/TokenReceiveScreen/index.tsx
+++ src/container/TokenReceiveScreen/index.tsx
@@ -6,17 +6,22 @@ import { Layout } from '../../layout/Layout';
 import { inject, observer } from 'mobx-react';
 import QRCode from 'react-native-qrcode';
 import { store } from '../../constants/store';
+import { getAccountInfo } from '../../apis/ethers';
 
 interface TokenReceiveScreenProps {
   navigation: NavigationScreenProp<any, any>;
   walletStore: WalletStore;
 }
+var usrAddress = '';
 
 @inject(store.WALLET_STORE)
 @observer
 export class TokenReceiveScreen extends React.Component<
   TokenReceiveScreenProps
 > {
+  componentDidMount() {
+    usrAddress = getAccountInfo(this.props.walletStore!.getMnemonic, 0).address;
+  }
   render() {
     return (
       <Layout
@@ -24,13 +29,8 @@ export class TokenReceiveScreen extends React.Component<
         headerTitle="입금 주소"
         headerNavigation={this.props.navigation}
       >
-        <QRCode
-          value={this.props.walletStore.getWallet.address}
-          size={200}
-          bgColor="black"
-          fgColor="white"
-        />
-        <Text>{this.props.walletStore.getWallet.address}</Text>
+        <QRCode value={usrAddress} size={200} bgColor="black" fgColor="white" />
+        <Text>{usrAddress}</Text>
       </Layout>
     );
   }
