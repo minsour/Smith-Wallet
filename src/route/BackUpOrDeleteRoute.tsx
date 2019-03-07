6ad8d9d60161defa031b1f68c1ed71e35e63d463
dianalee <>
2019-03-07 23:47:26 +0900

    ENH: 니모닉 백업 담당하는 ManageMnemonicScreen으로 경로 수정

--- src/route/BackUpOrDeleteRoute.tsx
+++ src/route/BackUpOrDeleteRoute.tsx
@@ -1,10 +1,11 @@
-import { createSwitchNavigator } from "react-navigation";
-import { BackUpMnemonicScreen } from "../container/BackUpMnemonicScreen";
-import { BackUpQRScreen } from "../container/BackUpQRScreen";
-import { DeleteWalletScreen } from "../container/DeleteWalletScreen";
+import { createSwitchNavigator } from 'react-navigation';
+import { ManageMnemonicScreen } from '../container/ManageMnemonicScreen';
+import { BackUpQRScreen } from '../container/BackUpQRScreen';
+import { DeleteWalletScreen } from '../container/DeleteWalletScreen';
 
 export const BackUpOrDeleteRoute = createSwitchNavigator({
-  BackUpMnemonic: BackUpMnemonicScreen,
+  // BackUpMnemonic: BackUpMnemonicScreen,
+  ManageMnemonic: ManageMnemonicScreen,
   BackUpQRCode: BackUpQRScreen,
-  DeleteWallet: DeleteWalletScreen
+  DeleteWallet: DeleteWalletScreen,
 });
