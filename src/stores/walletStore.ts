bde6f69d91ffc27b8300a603f5c04429a6ecf639
DevSoopark <sparkm0128@gmail.com>
2019-03-17 15:51:13 +0900

    ENH: walletStore 수정

--- src/stores/walletStore.ts
+++ src/stores/walletStore.ts
@@ -2,9 +2,20 @@ import { observable, action, computed } from 'mobx';
 import { makePath, etherscanProvider } from '../apis/ethers';
 import { ethers } from 'ethers';
 import { walletTab } from '../constants/walletTab'
+import { string } from 'prop-types';
+import { type } from 'os';
+
+interface WalletStorage {
+  walletList: {}
+  Mnemonic: string
+  accountLength: number
+  accountPath: number[]
+  isLocked: boolean
+}
 
 interface Wallet {
-  wallet?: ethers.Wallet
+  walletAddress?: string
+  walletPrivateKey?: string
   totalBalance?: number
   accountName?: string
 }
@@ -16,41 +27,65 @@ export class WalletStore {
   }
     
   // interface 에는 modifier 가 안 붙여짐. 아래처럼 선언 할 때 붙이면 될 듯.
-  @observable public walletList: Map<string, Wallet> = new Map()
+  @observable public walletList: {} = {}
   @observable public Mnemonic: string = ''
   @observable public accountLength: number = 0
   @observable public accountPath: number[] = []
   @observable public currentWallet: Wallet = {}
-  @observable public pincode: string = ''
-  @observable public islocked: boolean = false
+  @observable public isLocked: boolean = false
+  @observable public walletStorage: WalletStorage = {
+    walletList: {},
+    Mnemonic: this.Mnemonic,
+    accountLength: this.accountLength,
+    accountPath: this.accountPath,
+    isLocked: this.isLocked,
+  }
+
+  @action public copyWalletStorage = () => {
+    this.walletList = this.walletStorage.walletList
+    this.Mnemonic = this.walletStorage.Mnemonic
+    this.accountLength = this.walletStorage.accountLength
+    this.accountPath = this.walletStorage.accountPath
+    this.isLocked = this.walletStorage.isLocked
+    console.log('뭐')
+    //console.log(this.walletList)
+    console.log('지')
+  }
   
   @action public setMnemonic = (Mnemonic: string) => {
     this.Mnemonic = Mnemonic
+    this.walletStorage.Mnemonic = Mnemonic
   }
 
   @action public addSmith = (path: number, name: string = '첫째 스미스') => {
     let wallet: Wallet = {
-      wallet: new ethers.Wallet(ethers.Wallet.fromMnemonic(this.Mnemonic, makePath(path)).privateKey, etherscanProvider),
+      walletAddress: ethers.Wallet.fromMnemonic(this.Mnemonic, makePath(path)).address,
+      walletPrivateKey: ethers.Wallet.fromMnemonic(this.Mnemonic, makePath(path)).privateKey,
       totalBalance: 0,
       accountName: name
     }
     this.accountPath.push(path)
     this.accountLength += 1
-    this.walletList.set(`${walletTab.Smith}${path}`, wallet)
-    console.log(this.walletList.get(walletTab.Smith))
-    console.log(this.walletList.get(walletTab.UPbit))
+    this.walletList[`${walletTab.Smith}${path}`] = wallet
+
+    this.walletStorage.walletList = this.walletList
+    this.walletStorage.accountPath = this.accountPath
+    this.walletStorage.accountLength = this.accountLength
   }
   
   @action public addUPbit = () => {
     console.log(this.walletList)
-    let wallet: Wallet = {
-    }
-    this.walletList.set(walletTab.UPbit, wallet)
-    console.log(wallet+' addWallet')
+    this.walletList[walletTab.UPbit] = true
+    this.walletStorage.walletList = this.walletList
+  }
+
+  @action public getWallet = (path: number) => {
+    return this.walletList[`${walletTab.Smith}${path}`]
   }
 
   @action public setWallet = (path: number) => {
-    this.currentWallet = this.walletList.get(`${walletTab.Smith}${path}`)!
+    this.currentWallet = this.getWallet(path)! //this.walletList.get(walletName)!
+    console.log(this.currentWallet)
     console.log(`${this.currentWallet.accountName}(${path}번째 지갑) 설정`)
   }
 }
