50dc8575918233d04b6300e80d7d0c799d55d247
dianalee <>
2019-03-21 04:17:54 +0900

    ENH: qrcode 밑 margin 스타일 추가

--- src/container/TokenReceiveScreen/index.tsx
+++ src/container/TokenReceiveScreen/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, Clipboard, Alert } from 'react-native';
 import { Text } from 'react-native-paper';
 import { NavigationScreenProp } from 'react-navigation';
 import { WalletStore } from '../../stores/walletStore';
@@ -6,10 +7,11 @@ import { Layout } from '../../layout/Layout';
 import { inject, observer } from 'mobx-react';
 import QRCode from 'react-native-qrcode';
 import { store } from '../../constants/store';
+import { styles } from '../TokenReceiveScreen/Styles';
 
 interface TokenReceiveScreenProps {
   navigation: NavigationScreenProp<any, any>;
-  walletStore: WalletStore;
+  walletStore?: WalletStore;
 }
 
 @inject(store.WALLET_STORE)
@@ -24,14 +26,26 @@ export class TokenReceiveScreen extends React.Component<
         headerTitle="입금 주소"
         headerNavigation={this.props.navigation}
       >
-        <QRCode
-          value={this.props.walletStore!.eoa.address}
-          size={200}
-          bgColor="black"
-          fgColor="white"
-        />
-        <Text>{this.props.walletStore!.eoa.address}</Text>
+        <View style={styles.QRCodeContainer}>
+          <QRCode
+            value={this.props.walletStore!.currentWallet.walletAddress}
+            size={200}
+            bgColor="black"
+            fgColor="white"
+          />
+        </View>
+        <Text onPress={this.copyWalletAddress}>
+          {this.props.walletStore!.currentWallet.walletAddress}
+        </Text>
       </Layout>
     );
   }
+  private copyWalletAddress = async () => {
+    if (this.props.walletStore!.currentWallet.walletAddress) {
+      await Clipboard.setString(
+        this.props.walletStore!.currentWallet.walletAddress,
+      );
+      Alert.alert('Copied!', 'Your wallet address is copied!');
+    }
+  };
 }
