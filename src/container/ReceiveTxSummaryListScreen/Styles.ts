c483be3964fd7840afd9d1398931e7e002252c0a
DianaLeee <yeonylee94@gmail.com>
2019-03-24 20:51:00 +0900

    ENH: 스타일 변경

--- src/container/ReceiveTxSummaryListScreen/Styles.ts
+++ src/container/ReceiveTxSummaryListScreen/Styles.ts
@@ -3,7 +3,11 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   listSectionContainer: {
     width: '100%',
+    height: '100%',
     margin: 5,
     marginLeft: -5,
+  },
+  valueText: {
+    marginTop: 10,
   },
 });
